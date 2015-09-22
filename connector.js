44a45597048028a2270fd64fc5311808562c1f07
Sqrt of Saturn <sqrt@octoblu.com>
2015-09-22 15:10:17 -0700

    v1.1.0 updated connector

--- connector.js
+++ connector.js
@@ -1,12 +1,12 @@
 'use strict';
-var Plugin = require('./index').Plugin;
-var util = require('util');
+var Plugin       = require('./index').Plugin;
+var util         = require('util');
 var EventEmitter = require('events').EventEmitter;
-var meshblu = require('meshblu');
-var _ = require("underscore");
-var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }
+var meshblu      = require('meshblu');
+var packageJSON  = require('./package.json');
+var _            = require('lodash');
 
-var URL;
+var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }
 
 var Connector = function(config){
   var self = this;
@@ -16,8 +16,14 @@ var Connector = function(config){
   self.onMessage = bind(self.onMessage, self);
   self.onConfig = bind(self.onConfig, self);
   self.run = bind(self.run, self);
-  self.consoleError = bind(self.consoleError, self);
-  process.on('uncaughtException', self.consoleError);
+  self.emitError = bind(self.emitError, self);
+  if(!process){
+    return;
+  }
+  process.on('uncaughtException', function(error){
+    self.emitError(error);
+    process.exit(1);
+  });
   return self;
 };
 
@@ -31,8 +37,8 @@ Connector.prototype.createConnection = function(){
     uuid   : self.config.uuid,
     token  : self.config.token
   });
-  self.conx.on('notReady', self.consoleError);
-  self.conx.on('error', self.consoleError);
+  self.conx.on('notReady', self.emitError);
+  self.conx.on('error', self.emitError);
 
   self.conx.on('ready', self.onReady);
   self.conx.on('message', self.onMessage);
@@ -45,7 +51,7 @@ Connector.prototype.onConfig = function(device){
   try{
     self.plugin.onConfig.apply(self.plugin, arguments);
   }catch(error){
-    self.consoleError(error);
+    self.emitError(error);
   }
 };
 
@@ -55,7 +61,7 @@ Connector.prototype.onMessage = function(message){
   try{
     self.plugin.onMessage.apply(self.plugin, arguments);
   }catch(error){
-    self.consoleError(error);
+    self.emitError(error);
   }
 };
 
@@ -63,110 +69,46 @@ Connector.prototype.onReady = function(){
   var self = this;
   self.conx.whoami({uuid: self.config.uuid}, function(device){
     self.plugin.setOptions(device.options || {});
-    console.log(device);
-
-    if(_.has(device, "cam_url")){
-      URL = device.cam_url;
-      var SURL = "<a href='" + URL + "'>Click here to view camera feed!</a>";
-
-    var  OPTIONS_FORM = [{
-        "type": "help",
-        "helpvalue": SURL
-      }, "interval"
-    ];
-
-  var  OPTIONS_SCHEMA = {
-      "type": "object",
-      "properties": {
-        "interval": {
-          "type": 'number',
-          "description": SURL ,
-          "enum": [1500,2000, 3000, 4000, 5000, 6000, 10000],
-          "required": true,
-          "default": 1500
-        }
-      }
-    };
-
+    var oldRecentVersions = device.recentVersions || [];
+    var recentVersions = _.union(oldRecentVersions, [packageJSON.version]);
     self.conx.update({
-      uuid: self.config.uuid,
-      token: self.config.token,
+      uuid:          self.config.uuid,
+      token:         self.config.token,
       messageSchema: self.plugin.messageSchema,
-      optionsSchema: OPTIONS_SCHEMA,
-      optionsForm: OPTIONS_FORM,
-      options: self.plugin.options,
-      cam_url: URL
+      optionsSchema: self.plugin.optionsSchema,
+      options:       self.plugin.options,
+      initializing:  false,
+      currentVersion: packageJSON.version,
+      recentVersions: recentVersions
     });
-    }else{
+  });
+};
 
-      self.conx.register({},function(result) {
-        console.log("okay okay heres a subdevice", result.uuid);
-        URL = "https://s3-us-west-2.amazonaws.com/octoblu.com/camera.html?uuid=" + result.uuid + "&token=" + result.token + "&camera=" + self.config.uuid;
+Connector.prototype.run = function(){
+  var self = this;
+  self.plugin = new Plugin();
+  self.createConnection()
+  self.plugin.on('data', function(data){
+    self.emit('data.send', data);
+    self.conx.data(data);
+  });
 
+  self.plugin.on('error', self.emitError);
 
+  self.plugin.on('update', function(properties){
+    self.emit('update', properties);
+    self.conx.update(properties);
+  });
 
-          var SURL = "<a href='" + URL + "'>Click here to view camera feed!</a>";
+  self.plugin.on('message', function(message){
+    self.emit('message.send', message);
+    self.conx.message(message);
+  });
+};
 
-        var  OPTIONS_FORM = [{
-            "type": "help",
-            "helpvalue": SURL
-          }, "interval"
-        ];
+Connector.prototype.emitError = function(error){
+  var self = this;
+  self.emit('error', error);
+};
 
-      var  OPTIONS_SCHEMA = {
-          "type": "object",
-          "properties": {
-            "interval": {
-              "type": 'number',
-              "description": SURL ,
-              "enum": [1500,2000, 3000, 4000, 5000, 6000, 10000],
-              "required": true,
-              "default": 1500
-            }
-          }
-        };
-
-        self.conx.update({
-          uuid: self.config.uuid,
-          token: self.config.token,
-          messageSchema: self.plugin.messageSchema,
-          optionsSchema: OPTIONS_SCHEMA,
-          optionsForm: OPTIONS_FORM,
-          options: self.plugin.options,
-          cam_url: URL
-        });
-
-          });
-
-
-        }
-
-
-    });
-  };
-
-  Connector.prototype.run = function(){
-    var self = this;
-    self.plugin = new Plugin();
-    self.createConnection();
-    self.plugin.CameraOn();
-    self.plugin.on('data', function(data){
-      self.emit('data.send', data);
-      self.conx.data(data);
-    });
-
-    self.plugin.on('error', self.consoleError);
-
-    self.plugin.on('message', function(message){
-      self.emit('message.send', message);
-      self.conx.message(message);
-    });
-  };
-
-  Connector.prototype.consoleError = function(error){
-    var self = this;
-    self.emit('error', error);
-    console.error(error);
-  };
-
-  module.exports = Connector;
+module.exports = Connector;
--- index.js
+++ index.js
@@ -1,55 +1,68 @@
 'use strict';
-var util = require('util');
-var EventEmitter = require('events').EventEmitter;
-var debug = require('debug')('meshblu-cam')
-var av = require('audiovideo');
-var fs = require('fs');
-var base64Img = require('base64-img');
-var _ = require("underscore");
-var interval = 1500;
+var util          = require('util');
+var EventEmitter  = require('events').EventEmitter;
+var debug         = require('debug')('meshblu-cam')
+var MeshbluConfig = require('meshblu-config');
+var meshbluConfig = new MeshbluConfig({});
+var meshbluJSON   = meshbluConfig.toJSON();
+var av            = require('audiovideo');
+var fs            = require('fs');
+var base64Img     = require('base64-img');
+var _             = require("lodash");
+
+var SURL = "https://s3-us-west-2.amazonaws.com/octoblu.com/camera.html?uuid=" + meshbluJSON.uuid + "&token=" + meshbluJSON.token + "&camera=" + meshbluJSON.uuid;
 
 var MESSAGE_SCHEMA = {
   type: 'object',
-  properties: {
-    exampleBoolean: {
-      type: 'boolean',
-      required: true
+  properties: {}
+};
+
+var OPTIONS_FORM = [
+  {
+    "type": "help",
+    "helpvalue": SURL
+  },
+  "interval"
+];
+
+var OPTIONS_SCHEMA = {
+  "type": "object",
+  "properties": {
+    "interval": {
+      "type": 'number',
+      "description": SURL,
+      "enum": [1500, 2000, 3000, 4000, 5000, 6000, 10000],
+      "required": true,
+      "default": 1500
     }
   }
 };
 
-
 function Plugin(){
   this.options = {};
   this.messageSchema = MESSAGE_SCHEMA;
+  this.optionsSchema = OPTIONS_SCHEMA;
+  this.optionsForm = OPTIONS_FORM;
+  this.running = false;
   return this;
 }
 util.inherits(Plugin, EventEmitter);
 
 Plugin.prototype.CameraOn = function(){
-
-var self = this;
-
-av.acquireCamera(function (err, camera) {
-setInterval(function() {
-
-  camera.captureShot('jpeg').pipe(fs.createWriteStream('out.jpg'));
-
-  base64Img.base64('out.jpg', function(err, data) {
-    var image = data;
-    //console.log(image);
-    var payload =  {
-      "pictures": image
-    };
-
-    self.emit('message',{
-      devices: ['*'], payload: payload
+  var self = this;
+  self.running = true;
+  av.acquireCamera(function (err, camera) {
+    setInterval(function() {
+      camera.captureShot('jpeg').pipe(fs.createWriteStream('out.jpg'));
+      base64Img.base64('out.jpg', function(err, data) {
+        var image = data;
+        var payload =  {
+          "pictures": image
+        };
+        self.emit('message', { devices: ['*'], payload: {pictures: image} });
       });
-
-  })
-}, interval);
-});
-
+    }, self.options.interval);
+  });
 };
 
 Plugin.prototype.onMessage = function(message){
@@ -57,21 +70,22 @@ Plugin.prototype.onMessage = function(message){
 };
 
 Plugin.prototype.onConfig = function(device){
-  this.setOptions(device.options||{});
-
   var self = this;
+  self.setOptions(device.options||{});
 
-  interval = device.options.interval || 1500;
-
-
-
+  if(self.running){
+    return;
+  }
+  self.CameraOn();
 };
 
 Plugin.prototype.setOptions = function(options){
-  this.options = options;
+  this.options = _.defaults(options, {interval: 1500});
 };
 
 module.exports = {
   messageSchema: MESSAGE_SCHEMA,
+  optionsSchema: OPTIONS_SCHEMA,
+  optionsForm: OPTIONS_FORM,
   Plugin: Plugin
 };
