618a5d5a2a8c701fc187a09f97c4930e612e492a
virgilvox <moheeb.zara@citrix.com>
2015-10-02 01:13:11 -0700

    bump, and some modification to options and onmessage

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ var fs            = require('fs');
 var base64Img     = require('base64-img');
 var _             = require("lodash");
 
+var cameratwo;
+
 var SURL = "https://s3-us-west-2.amazonaws.com/octoblu.com/camera.html?uuid=" + meshbluJSON.uuid + "&token=" + meshbluJSON.token + "&camera=" + meshbluJSON.uuid;
 
 var MESSAGE_SCHEMA = {
-  type: 'object',
-  properties: {}
+  "type": 'object',
+  "properties": {
+    "snapshot": {
+      "type": "boolean",
+      "title": "Take picture",
+      "default": true
+    }
+  }
 };
 
 var OPTIONS_FORM = [
@@ -34,6 +42,11 @@ var OPTIONS_SCHEMA = {
       "enum": [1500, 2000, 3000, 4000, 5000, 6000, 10000],
       "required": true,
       "default": 1500
+    },
+    "broadcast": {
+      "type": "boolean",
+      "title": "broadcast at interval?",
+      "default": false
     }
   }
 };
@@ -52,21 +65,35 @@ Plugin.prototype.CameraOn = function(){
   var self = this;
   self.running = true;
   av.acquireCamera(function (err, camera) {
+    cameratwo = camera;
     setInterval(function() {
-      camera.captureShot('jpeg').pipe(fs.createWriteStream('out.jpg'));
-      base64Img.base64('out.jpg', function(err, data) {
-        var image = data;
-        var payload =  {
-          "pictures": image
-        };
-        self.emit('message', { devices: ['*'], payload: {pictures: image} });
-      });
+      if(self.options.broadcast){
+        camera.captureShot('jpeg').pipe(fs.createWriteStream('out.jpg'));
+        base64Img.base64('out.jpg', function(err, data) {
+          var image = data;
+          var payload =  {
+            "pictures": image
+          };
+          self.emit('message', { devices: ['*'], payload: {pictures: image} });
+        });
+      }
     }, self.options.interval);
   });
 };
 
 Plugin.prototype.onMessage = function(message){
+  var self = this;
   var payload = message.payload;
+  if(payload.snapshot){
+    cameratwo.captureShot('jpeg').pipe(fs.createWriteStream('out.jpg'));
+    base64Img.base64('out.jpg', function(err, data) {
+      var image = data;
+      var payload =  {
+        "pictures": image
+      };
+      self.emit('message', { devices: ['*'], payload: {pictures: image} });
+    });
+   }
 };
 
 Plugin.prototype.onConfig = function(device){
